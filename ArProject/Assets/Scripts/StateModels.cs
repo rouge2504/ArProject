01f171f06778fc85110fa2191807ed5f0bba56ff
Rogelio Trejo <rtrejo@dreidel.mx>
2018-09-04 13:11:03 -0500

    Agragado de animacion de caminado y muerte en el modelo

---
+++ ArProject/Assets/Scripts/StateModels.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateModels : MonoBehaviour {
+	[SerializeField]
+	private GameObject model;
+	private Animator animator;
+
+	[SerializeField]
+	private AnimationClip walkingClip;
+	[SerializeField]
+    private AnimationClip dieClip;
+	// Use this for initialization
+	void Start () { 
+		animator = model.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.A)){
+			DieState();
+		}
+	}
+
+	public void DieState(){
+		animator.SetBool("shoot", true);
+	}
+}
