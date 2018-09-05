3eb2d08e774e453efca28a7a093b059cb9229ae4
Rogelio Trejo <rtrejo@dreidel.mx>
2018-09-05 16:17:37 -0500

    Falta el acceder a la variable

--- ArProject/Assets/Scripts/StateModels.cs
+++ ArProject/Assets/Scripts/StateModels.cs
@@ -6,20 +6,54 @@ public class StateModels : MonoBehaviour {
 
 	public GameObject model;
 	private Animator animator;
+
+	public GameObject[] _materials;
+
+	private Renderer[] renderers;
+
+	private bool startDissolve;
+
+	private float speed;
+
+	private float dissolve;
     
 	// Use this for initialization
-	void Start () { 
+	void Start () {
+		dissolve = 0;
+		speed = 0;
 		animator = model.GetComponent<Animator>();
+		startDissolve = false;
+
+		renderers = new Renderer[_materials.Length];
+		for (int i = 0; i < _materials.Length; i++){
+			renderers[i] = _materials[i].GetComponent<Renderer>();
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.A)){
+		/*if (Input.GetKeyDown(KeyCode.A)){
 			DieState();
-		}
+		}*/
+		InitDissolve();
 	}
 
 	public void DieState(){
 		animator.SetBool("shoot", true);
+	}
+
+	public void Dissolve(){
+		startDissolve = true;
+	}
+
+	private void InitDissolve(){
+		if (startDissolve){
+			float dissolve = Shader.GetGlobalFloat("dissolve");
+			dissolve += Time.deltaTime;
+			for (int i = 0; i < _materials.Length; i++)
+            {
+				//dissolve = renderers[i].material.
+            }
+		}
 	}
 }
