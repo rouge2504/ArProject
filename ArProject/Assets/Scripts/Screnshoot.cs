965e7647dd38f472f9b20f352f7839ce1cefef38
Rogelio Trejo <rtrejo@dreidel.mx>
2018-09-14 09:24:18 -0500

    Puesta de boton screenshot

--- ArProject/Assets/Scripts/Screnshoot.cs
+++ ArProject/Assets/Scripts/Screnshoot.cs
@@ -6,11 +6,18 @@ public class Screnshoot : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		/*ScreenshotManager.OnScreenshotTaken += ScreenshotTaken;
+        ScreenshotManager.OnScreenshotSaved += ScreenshotSaved; */
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+
+	public void OnSaveImagePress()
+    {
+		ScreenshotManager.SaveScreenshot("MyScreenshot", "FIDH", "jpeg");
+    }
 }
