e341cec163beacde790d5f5ca7b587cdb3c7ac56
Rogelio Trejo Perez <rouge2504@gmail.com>
2018-12-17 11:34:34 -0600

    Puesta de las paginas para navegar

--- ArProject/Assets/Scripts/MenuManager.cs
+++ ArProject/Assets/Scripts/MenuManager.cs
@@ -14,10 +14,19 @@ public class MenuManager : MonoBehaviour {
     private float timeTitle;
     private float timingTitle;
 
-    public RawImage raw;
- 
-    IClock recordingClock;
-    WebCamTexture cameraTexture;
+
+    Gyroscope m_Gyro;
+
+    [SerializeField]
+    private GameObject baseGO;
+
+    [SerializeField]
+    private GameObject placaGO;
+
+    //public RawImage raw;
+
+    //IClock recordingClock;
+    //WebCamTexture cameraTexture;
     // Use this for initialization
     void Start () {
         timeTitle = 4;
@@ -29,9 +38,9 @@ public class MenuManager : MonoBehaviour {
         menus[0].SetActive(true);
         vuforia.enabled = false;
 
-        cameraTexture = new WebCamTexture();
-        cameraTexture.Play();
-        raw.texture = cameraTexture;
+        //cameraTexture = new WebCamTexture();
+        //cameraTexture.Play();
+        //raw.texture = cameraTexture;
 	}
 	
 	// Update is called once per frame
@@ -65,43 +74,63 @@ public class MenuManager : MonoBehaviour {
             menus[i].SetActive(false);
         }
         page.SetActive(true);
-        vuforia.enabled = true;
     }
 
-    IEnumerator RecorderCourutine()
+    public void GetPageVuforia(GameObject page)
     {
-        print("Empezando a grabar");
-        recordingClock = new RealtimeClock();
-
-        NatCorder.StartRecording(Container.MP4, VideoFormat.Screen, AudioFormat.None, OnVideo);
-        // Wait for 10 seconds
-        StartCoroutine(RecordLoop());
-        yield return new WaitForSeconds(10f);
-        // Stop recording
-        // The `OnVideo` callback will be invoked with the path to the recorded video
-        NatCorder.StopRecording();
-        print("GRabado");
-    }
-
-    IEnumerator RecordLoop()
-    {
-        while (NatCorder.IsRecording)
+        for (int i = 0; i < menus.Length; i++)
         {
-            // Acquire an encoder frame from NatCorder
-            var frame = NatCorder.AcquireFrame();
-            // Blit the current camera preview frame to the encoder frame
-            Graphics.Blit(cameraTexture, frame);
-            // Commit the frame to NatCorder for encoding
-            NatCorder.CommitFrame(frame, recordingClock.CurrentTimestamp);
-            // Wait for the end of the application frame
-            yield return new WaitForEndOfFrame();
+            menus[i].SetActive(false);
+        }
+        page.SetActive(true);
+        vuforia.enabled = true;
+        if (m_Gyro.enabled)
+        {
+            placaGO.SetActive(true);
+            baseGO.SetActive(false);
+        }
+        else
+        {
+            placaGO.SetActive(false);
+            baseGO.SetActive(true);
         }
     }
 
-    public void Recorder()
-    {
-        StartCoroutine(RecorderCourutine());
-    }
+    /* IEnumerator RecorderCourutine()
+     {
+         Debug.Log("Empezando a grabar");
+         recordingClock = new RealtimeClock();
+
+         NatCorder.StartRecording(Container.MP4, VideoFormat.Screen, AudioFormat.None, OnVideo);
+         // Wait for 10 seconds
+         StartCoroutine(RecordLoop());
+         yield return new WaitForSeconds(10f);
+         // Stop recording
+         // The `OnVideo` callback will be invoked with the path to the recorded video
+         NatCorder.StopRecording();
+         Debug.Log("GRabado");
+     }
+
+     IEnumerator RecordLoop()
+     {
+         while (NatCorder.IsRecording)
+         {
+             // Acquire an encoder frame from NatCorder
+             var frame = NatCorder.AcquireFrame();
+             // Blit the current camera preview frame to the encoder frame
+             Graphics.Blit(cameraTexture, frame);
+             // Commit the frame to NatCorder for encoding
+             NatCorder.CommitFrame(frame, recordingClock.CurrentTimestamp);
+             // Wait for the end of the application frame
+             yield return new WaitForEndOfFrame();
+         }
+     }
+
+     public void Recorder()
+     {
+         Debug.Log("Entrando en la funcion Recorder");
+         StartCoroutine(RecorderCourutine());
+     }*/
 
 
     void OnVideo(string path)
