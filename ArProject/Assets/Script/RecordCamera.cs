4bdaa5d090e723391b779618dc62d82c66af3e80
Rogelio Trejo Perez <rouge2504@gmail.com>
2019-01-17 01:40:02 -0600

    Con grabadora solo falta moviles

---
+++ ArProject/Assets/Script/RecordCamera.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using UnityEngine.UI;
+using RenderHeads.Media.AVProMovieCapture;
+
+public class RecordCamera : MonoBehaviour
+{
+
+    // Status
+    private long _lastFileSize;
+    private uint _lastEncodedMinutes;
+    private uint _lastEncodedSeconds;
+    private uint _lastEncodedFrame;
+
+    public bool _whenRecordingAutoHideUI = true;
+    public bool _showUI = true;
+    public CaptureBase _movieCapture;
+    public void StartRecord()
+    {
+        StartCapture();
+    }
+
+    private void StartCapture()
+    {
+        _lastFileSize = 0;
+        _lastEncodedMinutes = _lastEncodedSeconds = _lastEncodedFrame = 0;
+        if (_whenRecordingAutoHideUI)
+        {
+            _showUI = false;
+        }
+        if (_movieCapture != null)
+        {
+            _movieCapture.StartCapture();
+        }
+    }
+}
